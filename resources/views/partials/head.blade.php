17b84109e0ed1d9e23bd4ac9ef312a18195dc63f
Wish Suharitdarmong <94657890+Peterwisu@users.noreply.github.com>
2022-03-26 06:04:59 +0000

    Add partial to posts folder and app.blade

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="base_url" content="{{ url('/') }}">
+
+    <title> FitMate-Demo</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/googlemap.js') }}" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/googlemap.css') }}" rel="stylesheet">
+    
+
+
+</head>
