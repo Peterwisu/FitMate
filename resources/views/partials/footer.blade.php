e59e3872f43d621f17aae1eb998493dd5ff26a05
Hamza Arou <hamzaaroussicomputing@gmail.com>
2022-04-17 22:43:40 +0100

    add contact page front end

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,39 +1,15 @@
 <footer class="bg-light text-center text-lg-start">
-    <!-- Grid container -->
-    <div class="container p-4">
-        <!--Grid row-->
-        <div class="row">
-            <!--Grid column-->
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer text</h5>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-                    aliquam voluptatem veniam, est atque cumque eum delectus sint!
-                </p>
-            </div>
-            <!--Grid column-->
-
-            <!--Grid column-->
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer text</h5>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-                    aliquam voluptatem veniam, est atque cumque eum delectus sint!
-                </p>
-            </div>
-            <!--Grid column-->
-        </div>
-        <!--Grid row-->
-    </div>
-    <!-- Grid container -->
-
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2020 Copyright: {{config('app.name')}}
+    <!-- <hr> -->
+    <div class="p-3 row">
+        <div class="d-flex justify-space-between">
+            <div class="footer-menu">
+                <a href="#">Privacy Policy</a>
+                <a href="#" class="px-5">Terms of Use</a>
+            </div>
+            <div>© 2022 Copyright: {{config('app.name')}}</div>
+        </div>
+
     </div>
     <!-- Copyright -->
 </footer>
