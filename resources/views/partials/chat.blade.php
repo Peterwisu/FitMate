b46b99c12f47926de138cb479ca71e476b5652fe
Julianna Grefkowicz <cantarellaa00@gmail.com>
2022-05-16 20:33:11 +0100

    Chat frontend fixes

--- resources/views/partials/chat.blade.php
+++ resources/views/partials/chat.blade.php
@@ -7,44 +7,44 @@
     <div class="px-2 scroll">
         <div class="" id="messages"></div>
     </div>
-    <nav class="navbar bg-white navbar-expand-sm  justify-content-between">
+    <nav class="navbar bg-white navbar-expand-sm  justify-content-between" style="display: block">
         <div class="mx-2">
 
             <form id=message_form>
-                
-               
-                  
-                     
+
+
+
+
                         <input type="hidden" name="user_id" id='user_id' value="{{ auth()->user()->id }}">
                         <input type="hidden" name="username" id='username' value="{{ auth()->user()->name }}">
                         {{-- <input type="text" name="message" id="message_input" placeholder="Write a message ...."> --}}
 
-                  
 
-                                <textarea name="message" id="message_input" class="form-control"  style="width:100"cols="50" placeholder="Write a message ...."rows="2"></textarea>
-                           
-                            
 
-                                <button type="submit" class="btn btn-sub " id="message_send">Send Message</button>
+                                <textarea name="message" id="message_input" class="form-control" cols="50" placeholder="This is a general chat for all logged in users! Write a message here..."rows="2" style="height: 50px"></textarea>
+
+
+
+                                <button type="submit" class="btn btn-sub" id="message_send">Send</button>
                                 <button type='button'
-                                onclick="ToggleChat()" class="btn btn-warning " >Hide Chat</button>
-                            
-                        
-                                        
-             
+                                onclick="ToggleChat()" class="btn close-chat-btn " >X</button>
+
+
+
+
             </form>
         </div>
     </nav>
 </div>
-<div class="" id="show_button">
+<div class="chat-btn" id="show_button">
     <button type='button'
-                                onclick="ToggleChat()" class="btn btn-warning  "  >Show Chat</button>
-                            
+                                onclick="ToggleChat()" class="btn btn-sub" style="box-shadow: 1px 1px 5px 2px" >Show Chat</button>
+
 </div>
 <script>
 
 function ToggleChat(){
-    
+
     if($('#chat-div').hasClass('hidden')){
 
         $('#chat-div').removeClass('hidden');
